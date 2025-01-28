d35b27c8dad9c8fe049ac765baeaaabafc8c7d0a
CintiaSRRodrigues1983 <cintiasilveira_rosa@hotmail.com>
2025-01-28 16:49:43 -0300

    projeto pessoal

---
+++ app.js
@@ -0,0 +1,8 @@
+idade = prompt ('Por favor, digite sua idade.');
+if (idade >=18) {
+    console.log ('Parabéns, já pode tomar vinho comigo!');
+} else {
+    if (idade < 18) {
+        console.log ('Que pena, você ainda não pode beber comigo!');
+    }
+}
